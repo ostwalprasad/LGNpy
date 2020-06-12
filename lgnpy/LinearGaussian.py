f1f1956684972f99978784af805302690ae59266
Prasad Ostwal <prasadostwal@gmail.com>
2020-06-12 14:48:59 +0530

    refactored run_inference

--- lgnpy/LinearGaussian.py
+++ lgnpy/LinearGaussian.py
@@ -100,61 +100,9 @@ class LinearGaussian(Graph):
             self.inf_summary.round(4)
             .replace(pd.np.nan, "", regex=True)
             .replace(0, "", regex=True)
-           # .iloc[np.lexsort((self.inf_summary.index, self.inf_summary['Evidence'].values))]
         )
 
         return self.inf_summary
-
-    def run_inference(self, debug=True, return_results=True):
-        """
-        Run Inference on network with given evidences.
-        """
-
-        _log = log.setup_logger(debug=debug)
-        _log.debug("Started")
-
-        self.parameters = dict.fromkeys(self.nodes)
-        if all(x == None for x in self.evidences.values()):
-            _log.debug("No evidence was set. Proceeding without evidence")
-        root_nodes = [
-            x
-            for x in self.g.nodes()
-            if self.g.out_degree(x) >= 1 and self.g.in_degree(x) == 0
-        ]
-        print("initial root nods")
-        print(root_nodes)
-        self.calculated_means = copy.deepcopy(self.evidences)
-        self.calculated_vars = dict.fromkeys(self.nodes)
-        self.done_flags = dict.fromkeys(self.nodes)
-
-        for node in root_nodes:
-            self.done_flags[node] = True
-
-        while not all(x == True for x in self.done_flags.values()):
-            next_root_nodes = copy.deepcopy(root_nodes)
-            root_nodes = []
-            for node in next_root_nodes:
-                _log.debug(
-                    f"Calculating children of {node} : {list(self.g.succ[node].keys())}"
-                )
-                if self.calculated_means[node] == None:
-                    self.calculated_means[node] = self.mean[self.nodes.index(node)]
-                    _log.debug(
-                        f"Evidence was not available for node {node}, so took mean."
-                    )
-                for child in self.g.succ[node]:
-                    if self.done_flags[child] != True:
-                        (
-                            self.calculated_means[child],
-                            self.calculated_vars[child],
-                        ) = self.__get_node_values(child)
-                        _log.debug(f"\tcalculated for {child}")
-                        self.done_flags[child] = True
-                        root_nodes.append(child)
-                    else:
-                        _log.debug(f"\t{child} already calculated")
-
-        return self.__build_results()
 
     def __get_pure_root_nodes(self, graph):
         root_nodes = [
@@ -188,8 +136,7 @@ class LinearGaussian(Graph):
         log_instance.debug(f"Parent nodes used: {self.parameters[node]['node_values']}")
         log_instance.debug(f"Beta calculated: {self.parameters[node]['node_betas']}")
 
-
-    def run_inference2(self, debug=True, return_results=True):
+    def run_inference(self, debug=True, return_results=True):
         """
         Run Inference 2 on network with given evidences.
         """
@@ -204,10 +151,11 @@ class LinearGaussian(Graph):
         self.calculated_means = copy.deepcopy(self.evidences)
         self.calculated_vars = dict.fromkeys(self.nodes)
         self.done_flags = dict.fromkeys(self.nodes)
+
         it=0
         while not nx.is_empty(g_temp):
             it+=1
-            _log.debug(f"\n{'__'*10}\nIteration {it}")
+            _log.debug(f"\n{'_'*15}\nStep {it}")
             # self.draw_network("ssdf", g_temp)
             pure_children = self.__get_pure_root_nodes(g_temp)
             for child in pure_children:
@@ -218,31 +166,7 @@ class LinearGaussian(Graph):
                     _log.debug(f"Skipped Calculating:'{child}' as evidence is available.")
                 g_temp.remove_nodes_from(list(g_temp.pred[child]))
 
-        # i=0
-        # while not nx.is_empty(g2):
-        #     i+=1
-        #     if i >1:
-        #         break
-        #     #self.draw_network("ssdf", g2)
-        #     pure_root_nodes = self.__get_pure_root_nodes(g2)
-        #     print(f"Pure root nodes {pure_root_nodes}")
-        #     for node in pure_root_nodes:
-        #         self.done_flags[node] = True
-        #
-        #     for node in pure_root_nodes:
-        #         if not g2.has_node(node):
-        #             continue
-        #         if self.calculated_means[node] is None:
-        #             self.calculated_means[node] = self.mean[self.nodes.index(node)]
-        #             _log.debug(f"Evidence was not available for node {node}, so took mean.")
-        #         children = list(g2.succ[node])
-        #         for child in children:
-        #             if self.done_flags[child] != True:
-        #                 self.calculated_means[child],self.calculated_vars[child],= self.__get_node_values(child)
-        #                 _log.debug(f"calculated {child}', using parents {list(g2.pred[child])}")
-        #                 self.done_flags[child] = True
-        #             self.__remove_pred_edges(child,g2)
-        #         g2.remove_nodes_from(list(nx.isolates(g2)))
+
         return self.__build_results()
 
     def get_inference_results(self):
